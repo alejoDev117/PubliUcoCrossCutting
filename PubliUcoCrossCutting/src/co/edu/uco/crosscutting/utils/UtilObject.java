43ec3b2966015e92985dac6e54e317280a3372ce
alejoDev117 <alejandrodev117@gmail.com>
2023-04-25 21:33:51 -0500

    Se agrega un nuevo metodo de getDefaultObject a UtilObject

--- PubliUcoCrossCutting/src/co/edu/uco/crosscutting/utils/UtilObject.java
+++ PubliUcoCrossCutting/src/co/edu/uco/crosscutting/utils/UtilObject.java
@@ -6,5 +6,8 @@ public interface UtilObject {
 	 static <O> boolean isNull(final O object) {
 		return (object == null);
 	}
-
+	
+	static <O> O getDefault(final O object, final O defaultValue) {
+		return isNull(object) ? defaultValue : object;
+	}
 }
